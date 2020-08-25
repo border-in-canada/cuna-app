6cc3b652b48616fab7ff60508e5fd61e4944e9c9
border-in-canada <mathesd84@gmail.com>
2020-08-25 16:57:02 -0700

    Configured form controls, overall state, and data handling

--- src/containers/ApplicationForm/ApplicationForm.js
+++ src/containers/ApplicationForm/ApplicationForm.js
@@ -1,84 +1,103 @@
 import React, { Component } from 'react';
 import styles from './ApplicationForm.module.css';
-import { Form } from 'react-formalized';
-import Input from '../../components/UI/Inputs/Input';
+import { Form, Input } from 'react-formalized';
 
 class ApplicationForm extends Component {
     state = {
-        purchasePrice: {
-            elementType: 'input',
-            type: 'text',
-            placeholder: 'Auto Purchase Price',
-            validation: {
-               required: true,
-               number: true
-            }
-        },
-        make: {
-            elementType: 'input',
-            type: 'text',
-            placeholder: 'Auto Make',
-            validation: {
-               required: true,
-               number: false
-            }
-        },
-        model: {
-            elementType: 'input',
-            type: 'text',
-            placeholder: 'Auto Model',
-            validation: {
-               required: true,
-               number: false
-            }
-        },
-        income: {
-            elementType: 'input',
-            type: 'text',
-            placeholder: 'Estimated Yearly Income',
-            validation: {
-               required: true,
-               number: true
+        formIsValid: false,
+        orderForm: {
+            purchasePrice: {
+                elementType: 'input',
+                type: 'text',
+                identifier: 'purchasePrice',
+                placeholder: 'Auto Purchase Price',
+                validation: {
+                   required: true,
+                   number: true
+                },
             },
-            value: ''
-        },
-        creditScore: {
-            elementType: 'input',
-            type: 'text',
-            placeholder: 'Estimated Credit Score',
-            validation: {
-                customRules: [{
-                    evaluation: value => value >= 300 && value <= 850,
-                    message: 'Value must be from 300-850'
-                }],
-               required: true,
-               number: true
+            make: {
+                elementType: 'input',
+                type: 'text',
+                identifier: 'make',
+                placeholder: 'Auto Make',
+                validation: {
+                   required: true            }
+            },
+            model: {
+                elementType: 'input',
+                type: 'text',
+                identifier: 'model',
+                placeholder: 'Auto Model',
+                validation: {
+                   required: true            }
+            },
+            income: {
+                elementType: 'input',
+                type: 'text',
+                identifier: 'income',
+                placeholder: 'Estimated Yearly Income',
+                validation: {
+                   required: true,
+                   number: true
+                },
+                value: ''
+            },
+            creditScore: {
+                elementType: 'input',
+                type: 'text',
+                identifier: 'creditScore',
+                placeholder: 'Estimated Credit Score',
+                validation: {
+                    customRules: [{
+                        evaluation: value => value >= 300 && value <= 850,
+                        message: 'Value must be from 300-850'
+                    }],
+                   required: true,
+                   number: true
+                }
             }
         }
     }
 
+    onChangeHandler = (formState) => {
+        this.setState({formIsValid: formState.isValid});
+    }
+
+    onSubmitHandler = (event, formState) => {
+        event.preventDefault();
+        window.alert(JSON.stringify(formState, null, 3));
+    }
+
+
     render(){
         const formElementsArray = [];
-        for (let key in this.state) {
+        for (let key in this.state.orderForm) {
             formElementsArray.push({
                 id: key,
-                config: this.state[key]
+                config: this.state.orderForm[key]
             });
         }
 
         let form = (
-            <Form>
+            <Form
+            onChange={this.onChangeHandler}
+            onSubmit={this.onSubmitHandler} >
                 {formElementsArray.map(formElement => (
                     <Input 
-                    key={formElement.config.id}
+                    key={formElement.id}
                     elementType={formElement.config.elementType}
                     type={formElement.config.type} 
+                    identifier={formElement.config.identifier}
                     placeholder={formElement.config.placeholder}
                     validation={formElement.config.validation}
                     />            
                 ))}
+                {this.state.formIsValid ? <button type='submit'>Submit</button> :
+                <button type='submit' disabled>Submit</button> }
             </Form>
         )
+        
         return (
             <div className={styles.FormContainer}>
                 <h1>Auto Application</h1>
