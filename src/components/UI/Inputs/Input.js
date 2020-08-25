fecfc85cb890fd53f3d5bb42c3df33a4893017b1
border-in-canada <mathesd84@gmail.com>
2020-08-25 12:26:19 -0700

    Set up reusable input component using react-formalized library

---
+++ src/components/UI/Inputs/Input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Input, Numeric } from 'react-formalized';
+
+const input = (props) => {
+    let inputElement = null;
+    
+    switch(props.elementType) {
+        case ('input'):
+            inputElement = <Input 
+            type={props.type}
+            placeholder={props.placeholder}
+            required={props.required} 
+            value={props.value}
+            onChange={props.changed}/>;
+            break;
+        case ('numeric'):
+            inputElement = <Numeric 
+            type={props.type}
+            placeholder={props.placeholder}
+            required={props.required} 
+            value={props.value}
+            onChange={props.changed}/>;
+            break;
+        default:
+            inputElement = <Input 
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed}/>;
+    }
+    return (
+    <div>
+        {inputElement}
+    </div>
+)};
+
+export default input;
--- src/containers/ApplicationForm/ApplicationForm.js
+++ src/containers/ApplicationForm/ApplicationForm.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import styles from './ApplicationForm.module.css';
+import { Form } from 'react-formalized';
+import Input from '../../components/UI/Inputs/Input';
 
 class ApplicationForm extends Component {
     render(){
         let form = (
-            <input type='text' placeholder='text field' />
+            <Form>
+                <Input 
+                elementType='input'
+                type='text' 
+                placeholder='text field'
+                required='true'
+                value=''
+                />            
+            </Form>
         )
         return (
-            <div>
+            <div className={styles.FormContainer}>
                 <h1>Auto Application</h1>
                 {form}
             </div>
